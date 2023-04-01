3c71b4727d83a5c231f9cafd0f9b55a1b4ca581d
AndresCArroyave <127248844+AndresCArroyave@users.noreply.github.com>
2023-04-01 12:50:52 -0500

    diccionario

--- diccionarios.py
+++ diccionarios.py
@@ -26,3 +26,56 @@ datos1["Sexo"]="F"
 datos1["direccion"]="calle siempre viva"
 
 print(datos1)
+
+#METODOS GET() devuelve el elemento de la clave 
+print(datos1.get("Edad"))
+
+#Metodo Update(): actualizar el diccionario
+
+animales = {
+    "invertebrado": "gusano", "Vertebrados": "elefante",
+    "carnivoro": "leon", "omnivoro": "Raton"
+}
+
+marcas = {
+    "Tenis": "Nike","celular": "Nokia","carro": "mazda",
+    "colchones": "comodisimos"
+}
+
+animales.update(marcas)
+
+print(animales)
+
+print(marcas)
+
+#Metodos pop(). eliminar la clave pasada como argumento y su valor
+
+animales.pop("Vertebrados")
+
+print(animales)
+
+#imprimir solo claves del diccionario
+
+print(animales.keys())
+
+for clave in animales.keys():
+    print(clave)
+
+#imprimir solo los  valores 
+print(animales.values())
+
+for valor in animales.values():
+    print(valor)
+
+#imprimir: clave y valor
+
+print(animales.items())
+
+for clave, valor in animales.items():
+    print(clave, "->", valor)
+
+#eliminar todos los elementos del diccionario
+
+animales.clear()
+
+print(animales)
